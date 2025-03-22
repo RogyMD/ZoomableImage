a06147d01c36a455f3d4d965f35a41bcfa5a9bc1
Igor Bidiniuc <igorbidiniuc@gmail.com>
2025-03-22 01:39:46 +0000

    add ZoomableImage

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "ZoomableImage",
+    platforms: [
+      .iOS(.v17),
+      .macOS(.v14),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZoomableImage",
             targets: ["ZoomableImage"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ZoomableImage"),
 
--- Sources/ZoomableImage/ZoomableImage.swift
+++ Sources/ZoomableImage/ZoomableImage.swift
@@ -1,2 +1,51 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+@available(iOS 17.0, macOS 14.0, *)
+public struct ZoomableImage: View {
+  private let image: Image
+  private let contentMode: ContentMode
+  @Binding private var isZooming: Bool
+  
+  @GestureState private var magnifyValue: MagnifyGesture.Value?
+  private var scale: CGFloat {
+    magnifyValue?.magnification ?? 1
+  }
+  private var anchor: UnitPoint {
+    magnifyValue?.startAnchor ?? .center
+  }
+  
+  public init(
+    image: Image,
+    contentMode: ContentMode = .fit,
+    isZooming: Binding<Bool> = .constant(false)
+  ) {
+    self.image = image
+    self.contentMode = contentMode
+    _isZooming = isZooming
+  }
+  
+  public var body: some View {
+    GeometryReader { proxy in
+      ZStack {
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .scaleEffect(scale, anchor: anchor)
+          .gesture(
+            MagnifyGesture()
+            .updating($magnifyValue) { value, state, _ in
+              state = value
+            }
+          )
+          .animation(Self.zoomAnimation, value: scale)
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .edgesIgnoringSafeArea(.all)
+    }
+    .onChange(of: scale) { oldValue, newValue in
+      isZooming = scale != 1
+    }
+  }
+  
+  private static let zoomAnimation = Animation.bouncy(duration: 0.4, extraBounce: 0.1)
+}
